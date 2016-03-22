47ff961a150df140c9d0e8f4ad975567a4101e0e
fsaibene <fernandomsaibene@hotmail.com>
2016-03-22 20:17:48 -0300

    Clase2

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	Fernando Saibene
+	<?php
+	echo "<h1>Hola Mundo</h1>";
+	$nombre="Fernando";
+	echo $nombre;
+	//echo "<h1>$nombre</h1>";
+	//echo "<h1>".$nombre."</h1>";
+	//echo '<h1>$nombre</h1>';
+
+	$numero=17;
+	if($numero < 18)
+		printf("Es menor de edad");
+	else
+		printf("Es mayor de edad");
+
+	$arrayNuevo= array(1,2,3,5);
+	$arrayNuevo[]=66;
+	$arrayNuevo[2]=666;
+
+	foreach ($arrayNuevo as $num)
+	 {
+		echo "<br>".$num;
+		# code...
+	}
+	$arrayProducto = array("Pizza"=>20,"Cerveza"=>30,"Faina"=>10);
+	echo $arrayProducto["Pizza"];
+	var_dump($arrayProducto);
+	?>
+</body>
+</html>
